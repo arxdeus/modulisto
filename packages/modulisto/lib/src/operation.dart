70b43ed44b6ba972a0b9b7cca056cae6acc64a8e
arxdeus <git@arxdeus.dev>
2025-03-09 19:10:42 +0300

    feat: `Operation<T>` - special indirect unit that wraps around functions

---
+++ packages/modulisto/lib/src/operation.dart
@@ -0,0 +1,21 @@
+import 'package:meta/meta.dart';
+import 'package:modulisto/src/unit/trigger.dart';
+
+@internal
+typedef OperationRunnerMap = Map<Symbol, Trigger<Object?>>;
+
+@internal
+base mixin OperationRunner {
+  @internal
+  static final operationRunners = Expando<OperationRunnerMap>('operationRunners');
+
+  @protected
+  // ignore: non_constant_identifier_names
+  Future<T> Operation<T>(Symbol operationId, Future<T> Function() callback) async {
+    final result = await callback();
+    final operationsMap = operationRunners[this];
+    final relevantTrigger = operationsMap?[operationId];
+    relevantTrigger?.call(result);
+    return result;
+  }
+}
